195fff83c4ca1df38ae3164a7c4775a1f32a4d78
shabbir <shabbir.manasawala52@gmail.com>
2022-05-23 13:34:37 +0530

    More eth impl

--- src/main/java/io/ysf/springsecurityjwtconsumer/contollers/EthController.java
+++ src/main/java/io/ysf/springsecurityjwtconsumer/contollers/EthController.java
@@ -11,6 +11,7 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.utils.Convert;
 
 @RestController
 public class EthController {
@@ -23,21 +24,21 @@ public class EthController {
 	@GetMapping("/ethConnectWallet")
 	public ModelAndView ethConnectWallet() {
 		ModelAndView modelAndView = new ModelAndView("ethWallet");
-		String url = "HTTP://127.0.0.1:7545";
-		String ethAddressAccount1 = "0x14494c0A9ADd1314286d523C04F17F1D698722c0";
-		String ethPrivateKeyAccount1 = "0x0c51696558fb2ed5412ec432945a2939fe3fe13afdb69136c8251e3eeec4185b";
+//		String url = "HTTP://127.0.0.1:7545";
+//		String ethAddressAccount1 = "0x14494c0A9ADd1314286d523C04F17F1D698722c0";
+//		String ethPrivateKeyAccount1 = "0x0c51696558fb2ed5412ec432945a2939fe3fe13afdb69136c8251e3eeec4185b";
 
-//		String url = "https://kovan.infura.io/v3/1bc7f1f7e1404bf5a2a59e8937e741ca";
-//		String ethAddressAccount1 = "0x515Dba6A9c2f35baAa8ef5c4748819EFa0Ad6692";
-//		String ethPrivateKeyAccount1 = "0x568ae25188390df509f11da22eaf546070a6b7a21e9ecd94aaac58f2ec833bfe";
-//		
+		String url = "https://kovan.infura.io/v3/1bc7f1f7e1404bf5a2a59e8937e741ca";
+		String ethAddressAccount1 = "0x515Dba6A9c2f35baAa8ef5c4748819EFa0Ad6692";
+		String ethPrivateKeyAccount1 = "0x568ae25188390df509f11da22eaf546070a6b7a21e9ecd94aaac58f2ec833bfe";
+
 		Web3j client = Web3j.build(new HttpService(url));
 
 		try {
 			EthGetBalance balance = client.ethGetBalance(ethAddressAccount1, DefaultBlockParameter.valueOf("latest"))
 					.sendAsync().get(10, TimeUnit.SECONDS);
 			System.out.println("Balance: " + balance.getBalance());
-			modelAndView.addObject("balance", balance.getBalance());
+			modelAndView.addObject("balance", Convert.fromWei(balance.getBalance().toString(), Convert.Unit.ETHER));
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
